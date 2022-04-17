2a6b2444127da5eb65c50d150c470b0ade4e287a
JacksonBurns <JacksonBurns@users.noreply.github.com>
2022-04-16 22:12:28 -0400

    get version from __init__

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os.path
+import codecs
 import pathlib
 from setuptools import setup
 
@@ -7,10 +9,26 @@ cwd = pathlib.Path(__file__).parent
 # The text of the README file
 README = (cwd / "README.md").read_text()
 
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 # This call to setup() does all the work
 setup(
     name="py2sambvca",
-    version="1.1.0",
+    version=get_version("AIMSim/__init__.py"),
     description="Simple thin client to interface python scripts with SambVca catalytic pocket Fortran calculator.",
     long_description=README,
     long_description_content_type="text/markdown",
