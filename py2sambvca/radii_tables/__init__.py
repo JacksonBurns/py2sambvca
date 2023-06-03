d138ab7006a86957e6a9ad147a4e81b1d323987d
Jackson Burns <jwburns@mit.edu>
2023-06-03 15:33:19 -0400

    add default and van der waals radii options

--- py2sambvca/radii_tables/__init__.py
+++ py2sambvca/radii_tables/__init__.py
@@ -1,5 +1,8 @@
-from py2sambvca.radii_tables.default import radii_table as default
+from py2sambvca.radii_tables.default import default_radii_table
+from py2sambvca.radii_tables.vanDerWaals import vdw_radii_table
+from py2sambvca.radii_tables.format_table import format_radii_table
 
 table_lookup = {
-    "default": default,
+    "default": default_radii_table,
+    "vdw": vdw_radii_table,
 }
