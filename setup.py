a037cb9d72edf5641dd7617852b2f806fa92d8e6
JacksonBurns <jwburns@mit.edu>
2025-09-09 17:34:37 -0400

    add 3.13 badge

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     packages=["py2sambvca", "py2sambvca.radii_tables"],
     include_package_data=True
